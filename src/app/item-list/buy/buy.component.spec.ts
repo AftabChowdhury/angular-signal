eca9036ea38038edfbec01347387ec6e94bf8c94
Aftab Chowdhury <MMAftab.DE.Chowdhury@partner.bmw.de>
2024-02-07 13:43:05 +0100

    implement unit tests for buy.component  and sell.component

--- src/app/item-list/buy/buy.component.spec.ts
+++ src/app/item-list/buy/buy.component.spec.ts
@@ -1,23 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BuyComponent } from './buy.component';
+import { StateService } from '../../services/state.service';
 
 describe('BuyComponent', () => {
   let component: BuyComponent;
+  let stateService: StateService;
   let fixture: ComponentFixture<BuyComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BuyComponent]
+      imports: [BuyComponent],
+      providers: [StateService]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(BuyComponent);
     component = fixture.componentInstance;
+    stateService = TestBed.inject(StateService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call stateService.addQuantity with correct parameters', () => {
+    const itemId = 123;
+    const addQuantitySpy = spyOn(stateService, 'addQuantity');
+
+    component.addQuantity(itemId);
+
+    expect(addQuantitySpy).toHaveBeenCalledWith(itemId, 1);
   });
 });
